6ab32c5fbd4cfb089671650a8921e273fe3acf71
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2025-03-08 16:46:19 +0200

    feat: prompt user with a question and selectable options using p.select

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ import color from 'picocolors';
 
 let  correctTotal = 0;
 
-async function askQuestions(questions, answers, correctAnswerIndex) {
+async function askQuestions(question, answers, correctAnswerIndex) {
     
     const options = []
     answers.forEach((answer) => {
         options.push({value: answer, label: answer})
+    });
+
+    const answer = await p.select({
+        message: question,
+        initialValue: '1',
+        options: options,
     });
 }
